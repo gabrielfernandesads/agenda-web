95954be58775e5c249592de877025bfd79374911
gabriel <gabriel@k9>
2013-12-17 22:37:50 -0200

    Novo servlet!

---
+++ web-agenda/src/br/com/gabriel/servlet/OlaMundo.java
@@ -0,0 +1,22 @@
+package br.com.gabriel.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OlaMundo extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest requisicao, HttpServletResponse resposta)
+			throws ServletException, IOException {
+		PrintWriter out = resposta.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Ola Mundo: primeiro Servlet!!");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
